0867761e40bde32c66521eb2d21180780e4ebb66
Lucas Lanfredi <RM364773@fiap.com.br>
2026-01-04 20:40:54 -0300

    Corrigindo teste

--- src/main/java/fiap/lambda/WeeklyReportHandler.java
+++ src/main/java/fiap/lambda/WeeklyReportHandler.java
@@ -2,7 +2,6 @@ package fiap.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.CloudWatchLogsEvent;
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 import fiap.dto.WeeklyReport;
 import fiap.model.FeedbackEntity;
