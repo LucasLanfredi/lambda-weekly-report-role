2bdf99f256daf17157801046bb43be5ab26c2839
Lucas Lanfredi <RM364773@fiap.com.br>
2026-01-04 20:40:05 -0300

    Corrigindo teste

--- src/test/java/fiap/lambda/EmailServiceTest.java
+++ src/test/java/fiap/lambda/EmailServiceTest.java
@@ -1,14 +1,11 @@
 package fiap.lambda;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 import fiap.dto.WeeklyReport;
 import fiap.model.FeedbackEntity;
 import fiap.presenters.ReportPresenter;
 import org.junit.jupiter.api.Test;
-import software.amazon.awssdk.services.ses.SesClient;
-import software.amazon.awssdk.services.ses.model.SendEmailRequest;
 
 import java.util.List;
 import java.util.Map;
@@ -19,15 +16,15 @@ public class EmailServiceTest {
         void deveGerarRelatorioCorretamente() {
 
                 FeedbackEntity f1 = new FeedbackEntity();
-                f1.setNota(8);
+                f1.setNota(2); // URGENTE
                 f1.setTimestamp("2024-06-01T10:15:30.000Z");
 
                 FeedbackEntity f2 = new FeedbackEntity();
-                f2.setNota(6);
+                f2.setNota(6); // NÃO urgente
                 f2.setTimestamp("2024-06-01T14:20:00.000Z");
 
                 FeedbackEntity f3 = new FeedbackEntity();
-                f3.setNota(10);
+                f3.setNota(10); // NÃO urgente
                 f3.setTimestamp("2024-06-02T09:00:00.000Z");
 
                 List<FeedbackEntity> feedbacks = List.of(f1, f2, f3);
@@ -35,7 +32,7 @@ public class EmailServiceTest {
                 WeeklyReport report = ReportPresenter.gerarRelatorio(feedbacks);
 
                 // Média: (8 + 6 + 10) / 3 = 8.0
-                assertEquals(8.0, report.getMediaAvaliacoes());
+                assertEquals(6.0, report.getMediaAvaliacoes());
 
                 assertEquals(
                         Map.of(
@@ -47,8 +44,8 @@ public class EmailServiceTest {
 
                 assertEquals(
                         Map.of(
-                                Boolean.FALSE, 2L,
-                                Boolean.TRUE, 1L
+                                Boolean.TRUE, 1L,
+                                Boolean.FALSE, 2L
                         ),
                         report.getQuantidadePorUrgencia()
                 );
